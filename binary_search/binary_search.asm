2d359376c11937b18d86f64120debc6997e80729
Daniel Schäfer <s9dlscae@stud.uni-saarland.de>
2014-05-10 16:01:24 +0200

    binary Search working

--- binary_search/binary_search.asm
+++ binary_search/binary_search.asm
@@ -8,14 +8,16 @@ binary_search:
 	move $v0 $a0					# Adresse des ersten Wortes = Ergebnis
 	move $t0 $a0					# t0 = Adresse des ersten Wortes
 	move $t1 $a1		
-	addiu $t1 $t1 4					# t1 = Adresse des letzten Wortes
+	addiu $t1 $t1 -4				# t1 = Adresse des letzten Wortes
 	move $t2 $a2					# t2 = gesuchtes Wort
 	
 	
 calcmiddle:
-	beq $t0 $t1 PointerEven1			# Wenn Zeiger gleich, letzte Überprüfung
-	beq $t0 $t3 PointerEven1
-	beq $t1 $t3 PointerEven2
+
+	beq $t0 $t1 PointerEven			# Wenn Zeiger gleich, letzte Überprüfung
+	move $t5 $t1
+	addu $t5 $t5 -4
+	beq $t0 $t5 PointerEven
 	
 	addu $t3 $t0 $t1				
 	li $t5 2
@@ -23,23 +25,21 @@ calcmiddle:
 	mflo $t3						# t3 = Mitte
 	
 	#sll $t3 $t3 3
-	#srl $t3 $t3 1					# Teilbarkeit durch 4 mit Bitshifts sicherstellen
+	#sra $t3 $t3 1					# Teilbarkeit durch 4 mit Bitshifts sicherstellen (not working)	
 	
 	
-	#Neu
-	li $t5 4
+	li $t5 4						# Teilbarkeit mit 4 durch div und mul
 	div $t3 $t5
 	mflo $t3
 	mul $t3 $t3 4
-	#Ende
 	
 	
 	move $v0 $t3					# Mittelzeiger auf Gleichheit überprüfen
 	lw $t4 0($t3)
 	beq $t4 $t2 finish
-	
-	blt $t4 $t2 watchleft
-	bgt $t4 $t2 watchright
+	 
+	blt $t4 $t2 watchright
+	bgt $t4 $t2 watchleft
 	
 	
 watchleft:
@@ -50,19 +50,18 @@ watchright:
 	move $t0 $t3
 	b calcmiddle
 	
-PointerEven1: 
+PointerEven: 
 	move $v0 $t0	
-	lw $t4 0($t0)						# t4 = Wort auf das beide Zeiger zeigen
+	lw $t4 0($t0)					# t4 = Wort auf das beide Zeiger zeigen
 	beq $t4 $t2 finish				# bei Gleichheit mit gesuchtem Wort finish
-	li $v0 0						# Da keine Gleichheit Ergebnis = 0
-	b finish
 	
-PointerEven2: 
 	move $v0 $t1	
-	lw $t4 0($t1)						# t4 = Wort auf das beide Zeiger zeigen
+	lw $t4 0($t1)					# t4 = Wort auf das beide Zeiger zeigen
 	beq $t4 $t2 finish				# bei Gleichheit mit gesuchtem Wort finish
+	
 	li $v0 0						# Da keine Gleichheit Ergebnis = 0
 	b finish
+		
 	
 finish:
 	lw $ra 0($sp)
