e04f490da0d146ed906322831509b3276af3031a
Daniel Schäfer <s9dlscae@stud.uni-saarland.de>
2014-05-11 16:49:31 +0200

    apply not working yet

--- apply/apply.asm
+++ apply/apply.asm
@@ -1,5 +1,53 @@
 	.text
 	.globl apply
 apply:
-	# TODO implement
-	break
+	addiu	$sp $sp -20
+	sw	$s0 0($sp) 
+	sw	$s1 4($sp)
+	sw	$s2 8($sp)
+	sw	$s3 12($sp)
+	sw	$ra 16($sp)
+	
+	move $s0 $a0 
+	move $s1 $a1
+	beq $a0 $zero finish	# wenn erstes element nicht vorhanden, fertig?
+
+	
+loop:	
+	
+	lw $s2 0($s0)		# s2 = Adresse aufs Nachfolgeelement
+	lw $a0 4($s0)		# a0 = Inahlt des ersten Elements
+	
+	##
+	#sw	$s0 0($sp) 
+	#sw	$s1 4($sp)
+	#sw	$s2 8($sp)
+	#sw	$s3 12($sp)
+	#sw	$ra 16($sp)
+	##
+	
+	jalr $s1	
+	
+	##
+	#lw	$s0 0($sp)
+	#lw	$s1 4($sp)
+	#lw	$s2 8($sp)
+	#lw	$s3 12($sp)
+	#lw	$ra 16($sp)
+	##
+	
+	beq $s2 $zero finish	#Leere erste Adresse abfangen
+	move $s0 $s2	
+
+	b loop	
+
+
+
+finish:
+	lw	$s0 0($sp)
+	lw	$s1 4($sp)
+	lw	$s2 8($sp)
+	lw	$s3 12($sp)
+	lw	$ra 16($sp)
+	addiu	$sp $sp 20
+	jr $ra
